d84076e0baca02dabccc1bc24f3db2640a343e6d
AschleeOasis <JaydenKeithop@gmail.com>
2019-11-24 20:15:39 +0100

    third commit

--- dpll.cpp
+++ dpll.cpp
@@ -41,6 +41,52 @@ std::vector<std::vector<int>> parse_dimacs(std::string filename) {
 	return clauses;
 }
 
+
+/*DPLL algorithm: enumeration + propagagtion*/
+bool DPLL(std::vector<std::vector<int>> clauses)
+{
+	trail.clear();
+	if(!BCP()) return false;
+	while(true){
+		if(!decide()) return true;
+		while(!BCP())
+			if(!backtrack()) return false;
+	}
+
+}
+
+/*BCP(): Return false as soon as an unsatisfied clause is detected*/
+bool BCP(){
+	while(unit()) // there is a unit clause implying that a variable x must be set to a vaue v
+		trail.push(x,v,true);
+	if(unsatisfied()) return false;
+	return true;
+}
+
+/* decide(): assign new values to clause and return true, otherwise return false*/
+bool decide(){
+	if (all variables are assigned) return false;
+	choose unassigned variable x;
+	choose value v belongs to {0,1}
+	trail.push(x,v,false);
+	return true;
+}
+
+
+bool backtrack(){
+	while(true){
+		if(trail.empty()) return false;
+		(x, v, b) = trail.pop();
+		if(!b){
+			trail.push(x,not v, true);
+			return true;
+		}
+	}
+}
+
+
+
+
 int main(int argc, char* argv[]) {
 	assert(argc == 2);
 	auto clauses = parse_dimacs(argv[1]);
