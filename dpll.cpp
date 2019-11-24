ad8c8a5bae5e790a81f859f31dc1dca39b3045fa
AschleeOasis <JaydenKeithop@gmail.com>
2019-11-24 14:42:28 +0100

    Second commit

---
+++ dpll.cpp
@@ -0,0 +1,49 @@
+
+
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+/*Reconfigure the os function, set the output form to [a,b,c,d]*/
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+	os << "[";
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if (it != v.begin()) os << ", ";
+		os << *it;
+	}
+	return os << "]";
+}
+
+/*Read in CNF form file, and store cnf clauses into vector clauses.*/
+std::vector<std::vector<int>> parse_dimacs(std::string filename) {
+	std::vector<std::vector<int>> clauses;
+	std::ifstream in(filename);
+	std::string line;
+	while (!in.eof()) {
+		std::getline(in, line);
+		if (line == "") continue;
+		if (line.front() == 'c') continue;
+		if (line.front() == 'p') continue;
+		clauses.emplace_back();
+		const char* begin = line.c_str();
+		char* end = nullptr;
+		while (true) {
+			auto id = std::strtoll(begin, &end, 10);
+			begin = end;
+			if (id == 0) break;
+			clauses.back().emplace_back(id);
+		}
+		if (clauses.back().empty()) clauses.pop_back();
+	}
+	return clauses;
+}
+
+int main(int argc, char* argv[]) {
+	assert(argc == 2);
+	auto clauses = parse_dimacs(argv[1]);
+
+	std::cout << clauses << std::endl;
+}
